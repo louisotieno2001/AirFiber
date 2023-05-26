bc841e9992b6f14eec86b28bb31a3bf777038ce5
LouisO <louisotieno2001@gmail.com>
2023-05-26 11:12:52 +0300

    Basic files

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+//script for the navigation bar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  //script for body
+  
