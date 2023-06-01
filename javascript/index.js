6d3093f7e7be6dd9a12da142cbaadc0d4f9c50b4
LouisO <louisotieno2001@gmail.com>
2023-06-01 11:23:51 +0300

    Additional styles and carousel added

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,41 @@
-//script for the navigation bar
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function toggleMenu() {
+  var topnav = document.getElementById("myTopnav");
+  if (topnav.className === "topnav") {
+    topnav.className += " responsive";
+  } else {
+    topnav.className = "topnav";
   }
+}
 
-  //script for body
-  
+//Code for carousel
+
+const carouselContainer = document.querySelector('.carousel-container');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const totalItems = carouselItems.length;
+    let currentIndex = 0;
+
+    function showSlide(index) {
+      if (index < 0 || index >= totalItems) {
+        return;
+      }
+      carouselContainer.style.transform = `translateX(-${index * 100}%)`;
+      currentIndex = index;
+    }
+
+    function nextSlide() {
+      currentIndex++;
+      if (currentIndex >= totalItems) {
+        currentIndex = 0;
+      }
+      showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+      currentIndex--;
+      if (currentIndex < 0) {
+        currentIndex = totalItems - 1;
+      }
+      showSlide(currentIndex);
+    }
+
+    setInterval(nextSlide, 3000); 
